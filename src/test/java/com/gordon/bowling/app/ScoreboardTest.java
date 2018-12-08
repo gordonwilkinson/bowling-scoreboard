d82ecdf98665e279289cdcfd787640588a750069
Gordon Wilkinson <gordon.p.wilkinson@gmail.com>
2018-12-08 12:04:41 -0800

    initial unit test struct

---
+++ src/test/java/com/gordon/bowling/app/ScoreboardTest.java
@@ -0,0 +1,21 @@
+package com.gordon.bowling.app;
+
+import org.junit.Before;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by gordonwilkinson on 2018-12-08.
+ */
+
+public class ScoreboardTest {
+
+    private Scoreboard scoreboard;
+
+    @Before
+    public void verifyGameInitialization() {
+
+    }
+
+}
