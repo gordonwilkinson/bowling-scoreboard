d8a43025e2fe59b019f228367f9f2bc9efc12600
Gordon Wilkinson <gordon.p.wilkinson@gmail.com>
2018-12-08 16:27:38 -0800

    adding test service mocks

--- src/test/java/com/gordon/bowling/app/ScoreboardTest.java
+++ src/test/java/com/gordon/bowling/app/ScoreboardTest.java
@@ -1,25 +1,31 @@
 package com.gordon.bowling.app;
 
-
 import com.gordon.bowling.services.ConsoleService;
 import com.gordon.bowling.services.GameService;
 import com.gordon.bowling.services.ScoreboardService;
+import mockit.Expectations;
+import mockit.Injectable;
 import org.junit.Before;
 import org.junit.Test;
 
-
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.junit.Assert.assertThat;
+
 /**
  * Created by gordonwilkinson on 2018-12-08.
  */
 
 public class ScoreboardTest {
 
+    @Injectable
     GameService gameService;
-    ConsoleService consoleService;
-    ScoreboardService scoreboardService;
 
+    @Injectable
+    ConsoleService consoleService;
+
+    @Injectable
+    ScoreboardService scoreboardService;
 
     @Before
     public void verifyGameInitialization() {
@@ -27,12 +33,18 @@ public class ScoreboardTest {
         scoreboardService = new ScoreboardService();
         gameService = new GameService(consoleService, scoreboardService);
         assertThat(gameService, is(notNullValue()));
+
+
     }
 
     @Test
     public void verifyZeroScore() {
-
-
+        new Expectations() {{
+            int[][] scores = new int[10][3];
+            consoleService.getFrameScores();
+            result = scores;
+        }};
+        assertThat(gameService.getGameScore(), is(0));
     }
 
     @Test
